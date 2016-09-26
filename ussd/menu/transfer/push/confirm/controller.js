19a7221aa2095534e8afb8240e3ac2c561b23157
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-09-26 14:43:39 +0300

    feat: implement calls to identity service

--- ussd/menu/transfer/push/confirm/controller.js
+++ ussd/menu/transfer/push/confirm/controller.js
@@ -14,5 +14,18 @@ module.exports = {
       params.context = error
       return this.redirect('menu/user/wrongUri')
     })
+  },
+  receive: function (params) {
+    return this.bus.importMethod('identity.check')({
+      username: params.system.phone,
+      password: params.system.message
+    })
+    .then((result) => {
+      return params
+    })
+    .catch((error) => {
+      params.context = error
+      return this.redirect('menu/error/wrongPin')
+    })
   }
 }
