e03a0376732465c48d89afb98af178243bc8b7e1
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-15 15:55:27 +0200

    feat: bootstrap sell goods flow

--- ussd/menu/home/controller.js
+++ ussd/menu/home/controller.js
@@ -3,11 +3,24 @@ module.exports = {
     if (params.sourceAccount) {
       return params
     }
-    return this.bus.importMethod('ledger.account.get')({
+    return this.bus.importMethod('subscription.subscription.get')({
       phoneNumber: params.system.phone
     })
     .then((res) => {
+      return this.bus.importMethod('directory.user.get')({
+        URI: 'actor:' + res.actorId
+      })
+    })
+    .then((res) => {
+      params.name = res.name
+      return this.bus.importMethod('ledger.account.get')({
+        phoneNumber: params.system.phone
+      })
+    })
+    .then((res) => {
       params.sourceAccount = res.id
+      params.currencyCode = res.currencyCode
+      params.currencySymbol = res.currencySymbol
       return params
     })
     .catch(() => {
