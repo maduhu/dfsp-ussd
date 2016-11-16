06af2e210948e1077d38bbbc653d149cb06bb39d
Dimitar Timofeev <dimitar.timofeev@softwaregroup-bg.com>
2016-11-16 18:07:55 +0200

    feat: add screen for creating an invoice

---
+++ ussd/menu/invoice/amount/controller.js
@@ -0,0 +1,31 @@
+module.exports = {
+  send: function (params) {
+    if (params.invoice.destinationAccount) {
+      return params
+    }
+    return this.bus.importMethod('directory.user.get')({
+      URI: 'number:' + params.system.message
+    })
+    .then((result) => {
+      params.invoice.destinationName = result.name
+      params.invoice.destinationDfsp = result.dfsp
+      params.invoice.destinationUserNumber = params.system.message
+      return params
+    })
+    .catch((error) => {
+      params.context = error
+      return this.redirect('menu/user/wrongUri')
+    })
+  },
+  receive: function (params) {
+    if (Number.isNaN(parseFloat(params.system.message))) {
+      return this.redirect('menu/error/wrongInput')
+    }
+    if (params.system.input.requestParams.proceed) {
+      params.invoice.destinationAmount = params.system.message
+    } else {
+      delete params.invoice
+    }
+    return params
+  }
+}
