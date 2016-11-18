9804beb574ddea6ac1c42ac9f4e715524369e43d
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-18 19:15:30 +0200

    fix: prepare pending transactions menus

---
+++ ussd/menu/pendingTransactions/list/controller.js
@@ -0,0 +1,18 @@
+module.exports = {
+  send: function (params) {
+   if (params.pendingTransactions) {
+     return params
+   }
+   return this.bus.importMethod('transfer.invoiceNotification.fetch')({
+     userNumber: params.user.userNumber
+   }).then(function(res) {
+     if (Array.isArray(res) && res.length) {
+       params.pendingTransactions = res
+     }
+     return params
+   })
+  },
+  receive: function (params) {
+    return params
+  }
+}
