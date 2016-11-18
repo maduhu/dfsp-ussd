38537576e82228d977ac6b905c7d8b85d4090009
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-18 19:20:09 +0200

    fix: linting

--- ussd/menu/pendingTransactions/details/controller.js
+++ ussd/menu/pendingTransactions/details/controller.js
@@ -1,7 +1,7 @@
 module.exports = {
   send: function (params) {
-   // use params.system.requestParams.invoiceNotificationId to fetch invice info
-   return params
+    // use params.system.requestParams.invoiceNotificationId to fetch invice info
+    return params
   },
   receive: function (params) {
     return params
--- ussd/menu/pendingTransactions/list/controller.js
+++ ussd/menu/pendingTransactions/list/controller.js
@@ -1,16 +1,16 @@
 module.exports = {
   send: function (params) {
-   if (params.pendingTransactions) {
-     return params
-   }
-   return this.bus.importMethod('transfer.invoiceNotification.fetch')({
-     userNumber: params.user.userNumber
-   }).then(function(res) {
-     if (Array.isArray(res) && res.length) {
-       params.pendingTransactions = res
-     }
-     return params
-   })
+    if (params.pendingTransactions) {
+      return params
+    }
+    return this.bus.importMethod('transfer.invoiceNotification.fetch')({
+      userNumber: params.user.userNumber
+    }).then(function (res) {
+      if (Array.isArray(res) && res.length) {
+        params.pendingTransactions = res
+      }
+      return params
+    })
   },
   receive: function (params) {
     return params
