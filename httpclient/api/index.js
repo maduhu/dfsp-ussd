75aeda31382d5de03fb67a74657ebbf993a6d088
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-30 17:34:30 +0200

    feat: add more detailed logging to http clients

--- httpclient/api/index.js
+++ httpclient/api/index.js
@@ -1,7 +1,9 @@
 module.exports = {
   id: 'api',
   createPort: require('ut-port-jsonrpc'),
+  logLevel: 'trace',
   url: 'http://localhost:8010',
   namespace: ['directory', 'rule', 'transfer', 'account', 'ledger', 'subscription', 'wallet', 'spsp'],
+  logLevel: 'debug',
   method: 'post'
 }
