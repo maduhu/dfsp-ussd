fba1693142185d68241a271cf2db2abd19fa1b68
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-30 17:35:20 +0200

    fix: linting

--- httpclient/api/index.js
+++ httpclient/api/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   id: 'api',
   createPort: require('ut-port-jsonrpc'),
-  logLevel: 'trace',
   url: 'http://localhost:8010',
   namespace: ['directory', 'rule', 'transfer', 'account', 'ledger', 'subscription', 'wallet', 'spsp'],
   logLevel: 'debug',
