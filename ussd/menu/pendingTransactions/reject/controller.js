014fc5bb72f80ec0cf2dbe03c2e52381d7517c11
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-12-12 13:22:19 +0200

    fix: set status: rejected when rejecting a pending transaction

--- ussd/menu/pendingTransactions/reject/controller.js
+++ ussd/menu/pendingTransactions/reject/controller.js
@@ -1,6 +1,14 @@
 module.exports = {
   send: function (params) {
-    return params
+    return this.bus.importMethod('transfer.invoiceNotification.edit')({
+      invoiceNotificationId: params.pendingTransaction.invoiceNotificationId,
+      statusCode: 'r'
+    })
+    .then(result => params)
+    .catch((error) => {
+      params.context = error
+      return this.redirect('menu/user/wrongUri')
+    })
   },
   receive: function (params) {
     return params
