df9f3f2ed25a817603b38fc1c9e28a6f4c7f6785
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-12-16 16:59:06 +0200

    fix: store sourceAccountNumber in ussd session

---
+++ ussd/menu/account/select/controller.js
@@ -0,0 +1,17 @@
+module.exports = {
+  send: function (params) {
+    return params
+  },
+  receive: function (params) {
+    return this.bus.importMethod('ledger.account.get')({
+      accountNumber: params.system.input.requestParams.accountNumber
+    })
+    .then((res) => {
+      params.user.sourceAccount = res.id
+      params.user.currencyCode = res.currencyCode
+      params.user.currencySymbol = res.currencySymbol
+      params.user.sourceAccountNumber = params.system.input.requestParams.accountNumber
+      return params
+    })
+  }
+}
--- ussd/menu/home/controller.js
+++ ussd/menu/home/controller.js
@@ -9,6 +9,7 @@ module.exports = {
       phoneNumber: params.system.phone
     })
     .then((res) => {
+      params.user.actorId = res.actorId
       return this.bus.importMethod('directory.user.get')({
         actorId: res.actorId
       })
@@ -16,14 +17,26 @@ module.exports = {
     .then((res) => {
       params.user.userNumber = res.endUserNumber
       params.user.name = res.name
-      return this.bus.importMethod('ledger.account.get')({
-        phoneNumber: params.system.phone
+      return this.bus.importMethod('account.account.fetch')({
+        actorId: params.user.actorId
       })
     })
     .then((res) => {
-      params.user.sourceAccount = res.id
-      params.user.currencyCode = res.currencyCode
-      params.user.currencySymbol = res.currencySymbol
+      params.user.accounts = res
+      if (res.length === 1) {
+        var accountNumber = res[0].accountNumber
+        return this.bus.importMethod('ledger.account.get')({
+          accountNumber: accountNumber
+        }).then((res) => {
+          params.user.sourceAccount = res.id
+          params.user.currencyCode = res.currencyCode
+          params.user.currencySymbol = res.currencySymbol
+          params.user.sourceAccountNumber = accountNumber
+          return params
+        })
+      } else if (res.length > 1) {
+        return this.redirect('menu/account/select')
+      }
       return params
     })
     .catch(() => {
