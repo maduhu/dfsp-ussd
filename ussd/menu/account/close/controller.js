c4eda2ff2a42c75e18204e0b2accf7e0a75e32aa
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-01-11 16:03:20 +0200

    feat: close account

--- ussd/menu/account/close/controller.js
+++ ussd/menu/account/close/controller.js
@@ -28,7 +28,41 @@ module.exports = {
         password: params.system.message
       })
       .then((result) => {
-        return params
+        return this.bus.importMethod('ledger.account.edit')({
+          accountNumber: params.user.sourceAccountNumber,
+          isDisabled: true
+        })
+        .then((account) => {
+          return this.bus.importMethod('account.account.fetch')({
+            actorId: params.user.actorId
+          })
+        })
+        .then((res) => {
+          return this.bus.importMethod('ledger.account.fetch')({
+            accountNumber: res.map((el) => el.accountNumber)
+          })
+          .then((res) => {
+            params.user.accounts = res
+            if (res.length === 1) {
+              var accountNumber = res[0].accountNumber
+              return this.bus.importMethod('ledger.account.get')({
+                accountNumber: accountNumber
+              }).then((res) => {
+                params.user.sourceAccount = res.id
+                params.user.currencyCode = res.currencyCode
+                params.user.currencySymbol = res.currencySymbol
+                params.user.sourceAccountNumber = accountNumber
+                return params
+              })
+            } else if (res.length > 1) {
+              return this.redirect('menu/account/select')
+            }
+            return params
+          })
+        })
+        .catch(() => {
+          return this.redirect('menu/error/generic')
+        })
       })
       .catch((error) => {
         params.context = error
