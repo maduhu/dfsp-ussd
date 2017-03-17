79b2995d062f5025fb8f61ee74c2bb9a873c23cf
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-03-17 12:21:15 +0200

    feat: do not show change default DFPS menu if the current dfsp in not default or there is only one dfsp

--- ussd/menu/account/manage/controller.js
+++ ussd/menu/account/manage/controller.js
@@ -1,6 +1,19 @@
 module.exports = {
   send: function (params) {
-    return params
+    return this.bus.importMethod('ist.directory.user.list')({
+      identifier: params.user.identifier
+    })
+    .then((res) => {
+      params.defaultDFSP = res.list.find((el) => el.default)
+      if (res.currentDFSP === params.defaultDFSP.shortName) {
+        params.dfsp = res.list.filter((el) => !el.default)
+      }
+      return params
+    })
+    .catch((error) => {
+      params.context = error
+      return this.redirect('menu/error/generic')
+    })
   },
   receive: function (params) {
     return params
