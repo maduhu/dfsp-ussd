fc854d0755606c1f9cac3cac49d5d59d6a8144cb
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-04-04 19:12:52 +0300

    fix: remove identity dependency

--- httpclient/identity/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  id: 'identity',
-  createPort: require('ut-port-jsonrpc'),
-  url: 'http://localhost:8012',
-  namespace: ['identity'],
-  logLevel: 'debug',
-  method: 'post'
-}
