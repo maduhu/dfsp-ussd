a26903451ce65fd6e06334e3c239ee114514acc4
Ventsislav Dimitrov <wencislawdimitrow@gmail.com>
2017-07-13 13:24:05 +0300

    fix: fetch account on switch screen (#40)

--- ussd/menu/account/switch/controller.js
+++ ussd/menu/account/switch/controller.js
@@ -1,8 +1,20 @@
 var userHelper = require('../../../userHelper')
 module.exports = {
   send: function (params) {
-    params.user.availableAccounts = params.user.accounts.filter((acc) => acc.accountNumber !== params.user.sourceAccountNumber)
-    return params
+    return this.bus.importMethod('account.actorAccount.fetch')({
+      actorId: params.user.actorId
+    })
+      .then((r) => {
+        return this.bus.importMethod('ledger.account.fetch')({
+          accountNumber: r.map((el) => el.accountNumber)
+        })
+          .then((res) => {
+            params.user.accounts = userHelper.setAccountsStatus(r, res)
+            params.user.availableAccounts = params.user.accounts.filter((acc) => acc.accountNumber !== params.user.sourceAccountNumber)
+            return params
+          })
+          .catch(() => params)
+      })
   },
   receive: function (params) {
     if (!params.system.input.requestParams.accountNumber) {
