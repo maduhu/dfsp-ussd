198e1cb5eb4379dfcb672c1c7f17c1c0cc6edd68
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-08-03 13:23:28 +0300

    fix: change currency from usd to tzs

--- test/integration/test.004cashin.js
+++ test/integration/test.004cashin.js
@@ -11,7 +11,7 @@ const SECOND_OPTION = '2'
 const HOME = '0'
 const MINISTATEMENT = '6'
 const AGENT_MINISTATEMENT = '8'
-const AMOUNT = '42.00'
+const AMOUNT = '1242.00'
 
 test({
   type: 'integration',
