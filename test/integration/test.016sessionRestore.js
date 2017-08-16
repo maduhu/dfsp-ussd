a2fb611f62f8da6186c3a4ee87613f83a75e0e1f
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-08-16 17:22:50 +0300

    fix: add test for session expire

---
+++ test/integration/test.016sessionRestore.js
@@ -0,0 +1,94 @@
+/* eslint no-console: 0 */
+var test = require('ut-run/test')
+var commonFunc = require('./../lib/commonFunctions.js')
+var config = require('./../lib/appConfig')
+var joi = require('joi')
+const CUSTOMER = commonFunc.getCustomer('customer4')
+const INIT_MSG = '*123#'
+const FIRST_OPTION = '1'
+const SECOND_OPTION = '2'
+
+test({
+  type: 'integration',
+  name: 'Ussd service',
+  server: config.server,
+  serverConfig: config.serverConfig,
+  client: config.client,
+  clientConfig: config.clientConfig,
+  peerImplementations: config.peerImplementations,
+  steps: function (test, bus, run) {
+    return run(test, bus, [{
+      name: 'Login',
+      method: 'ussd.request',
+      params: {
+        phone: CUSTOMER.phoneNum,
+        message: INIT_MSG
+      },
+      result: (result, assert) => {
+        assert.equals(joi.validate(result, joi.object().keys({
+          shortMessage: joi.string(),
+          sourceAddr: joi.string(),
+          debug: joi.object().keys({
+            system: joi.object(),
+            user: joi.object(),
+            context: joi.object()
+          })
+        })).error, null, 'Log user - session resume')
+      }
+    },
+    {
+      name: 'Select send money after 60 seconds',
+      method: 'ussd.request',
+      params: () => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => resolve({
+            phone: CUSTOMER.phoneNum,
+            message: FIRST_OPTION
+          }), 60000)
+        });
+      },
+      result: (result, assert) => {
+        assert.equals(joi.validate(result, joi.object().keys({
+          shortMessage: joi.string(),
+          debug: joi.object().keys({
+            system: joi.object(),
+            user: joi.object(),
+            context: joi.object()
+          }),
+          sourceAddr: joi.any()
+        })).error, null, 'Select send money after 60 seconds')
+      }
+    },
+    {
+      name: 'Go to home menu',
+      method: 'ussd.request',
+      params: {
+        phone: CUSTOMER.phoneNum,
+        message: '0'
+      },
+      result: (result, assert) => {
+        assert.equals(joi.validate(result, joi.object().keys({
+          shortMessage: joi.string(),
+          debug: joi.object().keys({
+            system: joi.object(),
+            user: joi.object(),
+            context: joi.object()
+          }),
+          sourceAddr: joi.any()
+        })).error, null, 'Check home menu')
+      }
+    },
+    {
+      name: 'Close session',
+      method: 'ussd.closeSession',
+      params: {
+        phone: CUSTOMER.phoneNum
+      },
+      result: (result, assert) => {
+        assert.true(result.shortMessage.indexOf('Session Closed') > -1, 'Session Closed')
+      }
+    }
+    ]
+    )
+  }
+}, module.parent)
