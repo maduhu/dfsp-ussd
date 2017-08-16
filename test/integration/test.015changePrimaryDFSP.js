283d7bc61fa3ee0293cf916d2e79817f89cb2a9d
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-08-16 16:18:49 +0300

    fix: add tests on change primary dfsp

---
+++ test/integration/test.015changePrimaryDFSP.js
@@ -0,0 +1,151 @@
+/* eslint no-console: 0 */
+var test = require('ut-run/test')
+var commonFunc = require('./../lib/commonFunctions.js')
+var config = require('./../lib/appConfig')
+var joi = require('joi')
+const CUSTOMER = commonFunc.getCustomer('customer4')
+const INIT_MSG = '*123#'
+const FIRST_OPTION = '1'
+const SECOND_OPTION = '2'
+const SEVENTH_OPTION = '7'
+
+test({
+  type: 'integration',
+  name: 'Ussd service',
+  server: config.server,
+  serverConfig: config.serverConfig,
+  client: config.client,
+  clientConfig: config.clientConfig,
+  peerImplementations: config.peerImplementations,
+  steps: function (test, bus, run) {
+    return run(test, bus, [{
+      name: 'Login',
+      method: 'ussd.request',
+      params: {
+        phone: CUSTOMER.phoneNum,
+        message: INIT_MSG
+      },
+      result: (result, assert) => {
+        assert.equals(joi.validate(result, joi.object().keys({
+          shortMessage: joi.string(),
+          sourceAddr: joi.string(),
+          debug: joi.object().keys({
+            system: joi.object(),
+            user: joi.object(),
+            context: joi.object()
+          })
+        })).error, null, 'Log user - add account')
+      }
+    },
+    {
+      name: 'Select manage phone',
+      method: 'ussd.request',
+      params: {
+        phone: CUSTOMER.phoneNum,
+        message: SEVENTH_OPTION
+      },
+      result: (result, assert) => {
+        assert.equals(joi.validate(result, joi.object().keys({
+          shortMessage: joi.string(),
+          debug: joi.object().keys({
+            system: joi.object(),
+            user: joi.object(),
+            context: joi.object()
+          }),
+          sourceAddr: joi.any()
+        })).error, null, 'Select manage phone')
+      }
+    },
+    {
+      name: 'Select add user',
+      method: 'ussd.request',
+      params: {
+        phone: CUSTOMER.phoneNum,
+        message: SECOND_OPTION
+      },
+      result: (result, assert) => {
+        assert.equals(joi.validate(result, joi.object().keys({
+          shortMessage: joi.string(),
+          debug: joi.object().keys({
+            system: joi.object(),
+            user: joi.object(),
+            context: joi.object()
+          }),
+          sourceAddr: joi.any()
+        })).error, null, 'Check info in change primary dfsp')
+      }
+    },
+    {
+      name: 'Enter wrong pin',
+      method: 'ussd.request',
+      params: {
+        phone: CUSTOMER.phoneNum,
+        message: 'fail'
+      },
+      result: (result, assert) => {
+        assert.equals(joi.validate(result, joi.object().keys({
+          shortMessage: joi.string(),
+          debug: joi.object(),
+          sourceAddr: joi.any()
+        })).error, null, 'Check error pin message')
+      }
+    },
+    {
+      name: 'Return to enter pin',
+      method: 'ussd.request',
+      params: {
+        phone: CUSTOMER.phoneNum,
+        message: '1'
+      },
+      result: (result, assert) => {
+        assert.equals(joi.validate(result, joi.object().keys({
+          shortMessage: joi.string(),
+          debug: joi.object(),
+          sourceAddr: joi.any()
+        })).error, null, 'Pin confirmation')
+      }
+    },
+    {
+      name: 'Enter pin to confirm',
+      method: 'ussd.request',
+      params: {
+        phone: CUSTOMER.phoneNum,
+        message: CUSTOMER.pin
+      },
+      result: (result, assert) => {
+        assert.equals(joi.validate(result, joi.object().keys({
+          shortMessage: joi.string(),
+          debug: joi.object(),
+          sourceAddr: joi.any()
+        })).error, null, 'Pin confirmation')
+      }
+    },
+    {
+      name: 'Navigate to home screen',
+      method: 'ussd.request',
+      params: {
+        phone: CUSTOMER.phoneNum,
+        message: '0'
+      },
+      result: (result, assert) => {
+        assert.equals(joi.validate(result, joi.object().keys({
+          shortMessage: joi.string(),
+          debug: joi.object(),
+          sourceAddr: joi.any()
+        })).error, null, 'Check home screen')
+      }
+    },
+    {
+      name: 'Close session',
+      method: 'ussd.closeSession',
+      params: {
+        phone: CUSTOMER.phoneNum
+      },
+      result: (result, assert) => {
+        assert.true(result.shortMessage.indexOf('Session Closed') > -1, 'Session Closed')
+      }
+    }
+    ]
+    )
+  }
+}, module.parent)
